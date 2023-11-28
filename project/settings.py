efeeb8b6a7e6266610a60509cff98a0d07c1144b
Nikita <barskii.no@outlook.com>
2023-11-28 12:23:56 +0300

    Changes for start website locally, add readme

--- project/settings.py
+++ project/settings.py
@@ -1,21 +1,25 @@
 import os
+from environs import Env
+
+env = Env()
+env.read_env()
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
+        'HOST': env.str("DATABASE_HOST"),
         'PORT': '5434',
         'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'USER': env.str("DATABASE_USERNAME"),
+        'PASSWORD': env.str("DATABASE_PASSWORD"),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = env.str("SECRET_KEY")
 
-DEBUG = True
+DEBUG = env.bool("DEBUG")
 
 ROOT_URLCONF = 'project.urls'
 
